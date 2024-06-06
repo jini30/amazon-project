4d9ce4d07f11d0cc84eb70ddd3ca652e94fed67f
jini30 <mp348730@gmail.com>
2024-06-06 17:50:33 +0530

    Can choose quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) =>
                 </div>
 
                 <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                     <option selected value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -62,6 +62,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>
         button.addEventListener('click', () =>
             {
                 const productId = button.dataset.productId;
+                const productQuantityClass = `.js-quantity-selector-${productId}`;
+                const quantity = Number(document.querySelector(productQuantityClass).value);
                 let matchingItem;
 
                 cart.forEach((item) =>
@@ -75,14 +77,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>
 
                 if(matchingItem)
                 {
-                    matchingItem.quantity += 1;
+                    matchingItem.quantity += quantity;
                 }
                 else
                 {
                     cart.push(
                         {
                             productId: productId,
-                            quantity: 1
+                            quantity: quantity
                         }
                     );
                 }
