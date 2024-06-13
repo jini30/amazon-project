89317f19cf875d2c6c4493092facf6ffa501616f
jini30 <mp348730@gmail.com>
2024-06-13 22:50:06 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from "../../data/cart.js";
-import {products} from "../../data/products.js";
+import {getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import {deliveryOptions} from "../../data/deliveryOptions.js";
+import {getDeliveryOption, deliveryOptions} from "../../data/deliveryOptions.js";
 
 export function renderOrderSummary()
 {
@@ -13,27 +13,8 @@ export function renderOrderSummary()
   cart.forEach((cartItem) =>
     {
       const productId = cartItem.productId;
-      let matchingProduct;
-
-      products.forEach((product) =>
-        {
-          if(product.id == productId)
-          {
-            matchingProduct = product;
-          }
-        }
-      );
-
-      const deliveryOptionId = cartItem.deliveryOptionId;
-      let deliveryOption;
-      deliveryOptions.forEach((option) =>
-        {
-          if(option.id === deliveryOptionId)
-          {
-            deliveryOption = option;
-          }
-        }
-      );
+      const matchingProduct = getProduct(productId);
+      const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
       const today = dayjs();
       const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
       const dateString = deliveryDate.format('dddd, MMMM D');
