ab196721d3307c0d0e7731063a3ac711b474c1ce
jini30 <mp348730@gmail.com>
2024-06-17 22:50:39 +0530

    Create automated tests for addtoCart

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,26 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart)
+loadFromStorage();
+
+export function loadFromStorage()
 {
-    cart = [
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
-            quantity: 1,
-            deliveryOptionId: '2'
-        }
-    ];
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if(!cart)
+    {
+        cart = [
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
+                quantity: 1,
+                deliveryOptionId: '2'
+            }
+        ];
+    }
 }
 
 function saveToStorage()
@@ -32,7 +39,7 @@ export function calculateCartQuantity()
     return cartQuantity;
 }
 
-export function addToCart(productId, quantity)
+export function addToCart(productId, quantity=1)
 {
     let matchingItem;
 
